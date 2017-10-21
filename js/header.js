6578e2dda76851c8790fd89f36bf8dfba1d8bf8e
Franco <kinfaifrancochan@gmail.com>
2017-10-21 16:35:51 +0100

    centralise header links

--- js/header.js
+++ js/header.js
@@ -1,3 +1,11 @@
+// link list
+var linkList =
+  '<li><a href="index.html">home</a></li>'+
+  '<li><a href="about.html">about</a></li>'+
+  '<li><a href="services.html">services</a></li>'+
+  '<li><a href="our_plans.html">our plans</a></li>'+
+  '<li><a href="contact.html">contact</a></li>'
+
 // write header
 document.write(
 '<header class="noselect">'+
@@ -6,21 +14,14 @@ document.write(
       '<a href="index.html">EpicTuition</a>'+
     '</div>'+
     '<ul class="header__btn d-none d-md-block">'+
-      '<li><a href="index.html">home</a></li>'+
-      '<li><a href="about.html">about</a></li>'+
-      '<li><a href="services.html">services</a></li>'+
-      '<li><a href="our_plans.html">our plans</a></li>'+
-      '<li><a href="contact.html">contact</a></li>'+
+      linkList+
     '</ul>'+
     '<div class="header__menu-btn d-sm-block d-md-none">'+
       '<div class="header__menu-btn__line"></div>'+
     '</div>'+
     '<div class="header__menu">'+
       '<ul>'+
-        '<li><a href="about.html">about</a></li>'+
-        '<li><a href="services.html">services</a></li>'+
-        '<li><a href="our_plans.html">our plans</a></li>'+
-        '<li><a href="contact.html">contact</a></li>'+
+        linkList+
       '</ul>'+
       '<div class="header__menu__contact">'+
         '<i class="fa fa-mobile-phone"></i> 0774 928 4101<br>'+
@@ -34,10 +35,12 @@ document.write(
 var url = window.location.pathname;
 var fileName = url.substring(url.lastIndexOf('/')+1);
 var linkArray = document.getElementsByClassName('header__btn')[0].getElementsByTagName('a');
+var mobileLinkArray = document.getElementsByClassName('header__menu')[0].getElementsByTagName('a');
 for (var i=0; i<linkArray.length; i++) {
   var href = linkArray[i].getAttribute('href');
   if (fileName == href) {
     linkArray[i].classList.add('active');
+    mobileLinkArray[i].classList.add('active');
     break;
   }
 }
